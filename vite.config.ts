0fe1147b5a829a3ab7a098ad28aa28e91885ba9f
Springer <zxllks@yahoo.com>
2024-02-22 12:58:56 +0800

    add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 import Components from 'unplugin-vue-components/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -59,6 +60,30 @@ export default defineConfig({
         }
       ],
       dts: 'src/components.d.ts'
+    }),
+
+    // https://github.com/antfu/vite-plugin-pwa
+    // 针对 生产环境 需要打包
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'pk-front-vue3',
+        short_name: 'PK',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     })
   ],
   resolve: {
